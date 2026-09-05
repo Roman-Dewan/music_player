7bbec11cc6ebddda4bd8fde172e89d90a904809c
Roman-Dewan <roman.dewan18@gmail.com>
2026-09-05 14:54:55 +0600

    splash screen added

--- lib/features/auth/presentation/pages/splash_screen.dart
+++ lib/features/auth/presentation/pages/splash_screen.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:music_player/features/home/presentation/pages/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
   static const String name = "/splash-screen";
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 0.1,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        context.pushReplacement(HomeScreen.name);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Splash')),
+      body: Center(
+        child: ScaleTransition(
+          scale: _scaleAnimation,
+          child: SvgPicture.asset('assets/icon/music_icon.svg', width: 100),
+        ),
+      ),
     );
   }
 }
